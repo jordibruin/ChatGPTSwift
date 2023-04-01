ff3fb3e5ea84575922ae21ec52d78099220182e7
Jordi Bruin <jordibruin@gmail.com>
2023-04-01 11:33:11 +0200

    Update Models.swift

--- Sources/ChatGPTSwift/Models.swift
+++ Sources/ChatGPTSwift/Models.swift
@@ -106,3 +106,20 @@ struct StreamMessage: Decodable {
     let content: String?
     let role: String?
 }
+
+protocol Convertable: Codable {}
+extension Convertable {
+    func convertToDict() -> [String: Any] {
+        var dict: [String: Any]
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(self)
+            dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] ?? [:]
+        } catch {
+            print(error)
+            dict = [:]
+        }
+        
+        return dict
+    }
+}
